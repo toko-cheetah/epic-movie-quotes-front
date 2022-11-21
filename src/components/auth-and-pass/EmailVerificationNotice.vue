a314971cdedf503d4117caeaf67b290f2db9b55c
Tornike Khatiashvili <100285138+toko-cheetah@users.noreply.github.com>
2022-11-21 21:07:23 +0400

    wip: email verification

---
+++ src/components/auth-and-pass/EmailVerificationNotice.vue
@@ -0,0 +1,9 @@
+<template>
+  <BaseLayout>
+    <h1>Email verification notice</h1>
+  </BaseLayout>
+</template>
+
+<script setup>
+import BaseLayout from "@/components/auth-and-pass/BaseLayout.vue";
+</script>
